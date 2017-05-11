64be706596253bea5a31701971f28a42b668998c
SkokRider <chostr2007@gmail.com>
2017-05-11 19:23:23 +0300

    XD

--- PDFFinder/App.xaml.cs
+++ PDFFinder/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
 
 namespace PDFFinder
 {
@@ -15,14 +16,16 @@ namespace PDFFinder
     {
         void App_Startup(object sender, StartupEventArgs e)
         {
-            if(e.Args.Length==1)
+            if (e.Args.Length == 1)
             {
-                MessageBox.Show("Parameters");
+                //PROCESSname
+                //e.Args[1] - filename
+                string ProcessName = null;
+                string FileName = null;
+                Process.Start(ProcessName, FileName);
+                
             }
-            else
-            {
-                MessageBox.Show("Without parameters");
-            }
+           
         }
     }
 }
